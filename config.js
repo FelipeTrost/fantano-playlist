a6e4f2fad3a27dc96ad9bb5e7808b6eab1601ad6
Felipe Trost <felipe.trost@gmail.com>
2020-09-17 12:11:16 +0200

    Import config from env variables

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    youtube_key: process.env.youtube_key,
+    spotify_client_id: process.env.spotify_client_id,
+    spotify_redirect_url: process.env.spotify_redirect_url,
+    clientSecret: process.env.clientSecret,
+    mongoose_db: process.env.mongoose_db
+}
