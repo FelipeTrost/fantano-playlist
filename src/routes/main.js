3781204fd3e2c834b4c50c56937a69f8275d29bc
Felipe Trost <felipe.trost@gmail.com>
2020-09-22 19:36:49 +0200

    Fixed bug and added link to playlist

--- src/routes/main.js
+++ src/routes/main.js
@@ -33,29 +33,41 @@ router.get('/create_playlist', async (req, res) => {
         const existingFantanoPlaylist = users_playlists.body.items.find(playlist => playlist.name == targetPlaylistName);
 
         let targetPlaylistId;
+        let targetPlaylistUrl;
 
-        if (existingFantanoPlaylist)
+
+        if (existingFantanoPlaylist) {
             targetPlaylistId = existingFantanoPlaylist.id
-        else {
+            targetPlaylistUrl = existingFantanoPlaylist.external_urls.spotify
+        } else {
             const user_info = await spotifyApi.getMe();
             const username = user_info.body.display_name;
 
             const playlist = await spotifyApi.createPlaylist(username, targetPlaylistName);
             targetPlaylistId = playlist.body.id
+            targetPlaylistUrl = playlist.body.external_urls.spotify
         }
 
+        console.log(existingFantanoPlaylist)
         const fantanoTracks = await Track.find({
             spotify: true
         })
-
         const fantanoTracksUris = fantanoTracks.map(track => "spotify:track:" + track.spotify_id)
 
+
+        //First we call replace, this replaces all the tracks in the playlist with the ones we are giving it
+        //this way, if someone is adding the playlist for a second time, he doesn't get duplicates
+        await spotifyApi.replaceTracksInPlaylist(targetPlaylistId, fantanoTracksUris.splice(0, 100));
+
+
         //We can only put 100 songs at a time
-        for (let i = 0; i < Math.ceil(fantanoTracksUris.length / 100); i++) {
-            await spotifyApi.addTracksToPlaylist(targetPlaylistId, Array(...fantanoTracksUris).splice(0, 100));
+        const executions = Math.ceil(fantanoTracksUris.length / 100)
+        for (let i = 0; i < executions; i++) {
+            await spotifyApi.addTracksToPlaylist(targetPlaylistId, fantanoTracksUris.splice(0, 100));
         }
         res.json({
-            success: true
+            success: true,
+            targetPlaylistUrl
         })
     } catch (error) {
         console.log(error);
